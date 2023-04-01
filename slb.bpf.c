040b95ae4e49ed2484ded5f53a501fa026b54277
mageekchiu <mageekchiu@qq.com>
2023-04-01 22:07:58 +0800

    stage diff

--- slb.bpf.c
+++ slb.bpf.c
@@ -7,7 +7,7 @@ hm backends[NUM_BACKENDS] = {
 hm slb = {"172.19.0.5", 2896692485, 80};
 hm vip = {"172.19.0.10", 2896692490, 80};
 
-static __always_inline int parse_mac(struct xdp_md *ctx, struct ethhdr *eth ,struct iphdr *iph){
+static __always_inline int gen_mac(struct xdp_md *ctx, struct ethhdr *eth ,struct iphdr *iph){
     struct bpf_fib_lookup fib_params = {};
 	fib_params.family	= AF_INET;
     fib_params.tos		= iph->tos;
@@ -25,7 +25,7 @@ static __always_inline int parse_mac(struct xdp_md *ctx, struct ethhdr *eth ,str
         case BPF_FIB_LKUP_RET_SUCCESS:         /* lookup successful */
             memcpy(eth->h_dest, fib_params.dmac, ETH_ALEN);
             memcpy(eth->h_source, fib_params.smac, ETH_ALEN);
-            action = bpf_redirect(fib_params.ifindex, 0);
+            action = XDP_TX;
             break;
         case BPF_FIB_LKUP_RET_BLACKHOLE:    /* dest is blackholed; can be dropped */
         case BPF_FIB_LKUP_RET_UNREACHABLE:  /* dest is unreachable; can be dropped */
@@ -43,49 +43,116 @@ static __always_inline int parse_mac(struct xdp_md *ctx, struct ethhdr *eth ,str
     return action;
 }
 
+// todo random port within[30100,60900]
+__u16 cur = 0;
+static __always_inline __u16 get_port(){
+    __u16 t = cur++ + NAT_PORT_MIN;
+    if(t > NAT_PORT_MAX){
+        return 0;
+    }
+    return t;
+}
+
+// todo implement different load balancing algorithm
+int count = 0;
+static __always_inline hm *lb_rr(){
+    int backend_idx = count++ % NUM_BACKENDS;
+    return &(backends[backend_idx]);
+}
+static __always_inline hm *lb_rand(){
+    int backend_idx = bpf_get_prandom_u32() % NUM_BACKENDS;
+    return &(backends[backend_idx]);  
+}
+static __always_inline hm *get_backend(enum LB_ALG alg){
+    switch (alg){
+        case round_robin:
+            return lb_rr();   
+        case random: 
+        default:
+            return lb_rand();
+    }
+}
+
 SEC("xdp_slb")
 int xdp_load_balancer(struct xdp_md *ctx)
 {
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
-
-    bpf_printk("got a packet");
-
+    bpf_printk("Got a packet");
     struct ethhdr *eth = data;
     if (data + sizeof(struct ethhdr) > data_end)
-        return XDP_ABORTED;
+        return XDP_PASS;
 
-    if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
         return XDP_PASS;
 
     struct iphdr *iph = data + sizeof(struct ethhdr);
     if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
-        return XDP_ABORTED;
+        return XDP_PASS;
 
     if (iph->protocol != IPPROTO_TCP)
         return XDP_PASS;
 
-    bpf_printk("Got TCP packet from %x", iph->saddr);
+    struct tcphdr *tcph = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr) > data_end)
+        return XDP_PASS;
+
+    __u16 sport = bpf_ntohs(tcph->source);
+    __u16 dport = bpf_ntohs(tcph->dest);
+    __u16 tcp_len = bpf_ntohs(iph->tot_len) - (iph->ihl << 2);
+    bpf_printk("Got a TCP packet of tuple, from %x:%x to %x:%x, len", iph->saddr,sport,iph->daddr,dport,tcp_len);
+    if (tcp_len > TCP_MAX_BITS){
+        return XDP_DROP;
+    }
+    // __u16 new_src_port = sport;
+    // __u16 new_dest_port = dport;
 
     if (iph->daddr == vip.ip_int){
-        // Choose a backend server to send the request to
-        int backend_idx = bpf_get_prandom_u32() % NUM_BACKENDS;
-        hm rs = backends[backend_idx];
-        iph->daddr = rs.ip_int;
-        iph->saddr = slb.ip_int;
-        // *(eth->h_dest) = "ss";
-        // *(eth->h_source) = "ss";
+        // Choose a backend server to send the request to; 
+        hm *rs = get_backend(0);
+        ce nat_key = {
+            .ip = bpf_htonl(iph->saddr),
+            .port = sport
+        };
+        ce *nat_p = bpf_map_lookup_elem(&snat_map, &nat_key);
+        if (nat_p == NULL) {
+            ce nat_val = {
+                .ip = slb.ip_int,
+                .port = get_port()
+            };
+            nat_p = &nat_val;
+            bpf_map_update_elem(&snat_map, &nat_key, nat_p, map_flags); 
+            bpf_map_update_elem(&dnat_map, nat_p, &nat_key, map_flags);       
+        }
+        iph->daddr = rs->ip_int;
+        tcph->dest = rs->port;
+        // new_dest_port = rs->port;
+        iph->saddr = nat_p->ip;
+        tcph->source = nat_p->port;
+        // new_src_port = nat_p->port;
+        bpf_printk("Got a nat packet of tuple, from %x:%x to %x:%x", iph->saddr,sport,iph->daddr,dport);
     }else{
         // send resp to client
-        __u32 client_ip = 1;
-        iph->daddr = client_ip;
+        ce nat_key = {
+            .ip = bpf_htonl(iph->daddr),
+            .port = dport
+        };
+        ce *nat_p = bpf_map_lookup_elem(&snat_map, &nat_key);
+        if (nat_p == NULL) {
+            bpf_printk("No such connection from client before");
+            return XDP_DROP;
+        }
+        iph->daddr = nat_p->ip;
+        tcph->dest = nat_p->port;
+        // new_dest_port = nat_p->port;
         iph->saddr = vip.ip_int;
-        // *(eth->h_dest) = "ss";
-        // *(eth->h_source) = "ss";
+        tcph->source = vip.port;
+        // new_src_port = vip.port;
+        bpf_printk("Send a nat packet of tuple, from %x:%x to %x:%x", iph->saddr,sport,iph->daddr,dport);
     }
-    // bpf_fib_lookup();
-    int action = parse_mac(ctx,eth,iph);
+    int action = gen_mac(ctx,eth,iph);
     iph->check = iph_csum(iph);
+    tcph->check = ipv4_l4_csum(tcph, tcp_len, iph);
     return action;
 }
 
--- slb.h
+++ slb.h
@@ -3,8 +3,14 @@
 #include <linux/in.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <bpf_helpers.h>
-#include <bpf_endian.h>
+#include <linux/tcp.h>
+#include <linux/if_packet.h>
+#include <linux/types.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+#define TCP_MAX_BITS 1400
 
 #undef AF_INET
 #define AF_INET 2
@@ -15,6 +21,37 @@ typedef struct host_meta {
     __u32 ip_int;
     __u16 port;
 } hm;
+
+#define NAT_PORT_MIN 30100
+#define NAT_PORT_MAX 60900
+
+__u32 map_flags = BPF_ANY;
+
+struct conntrack_entry {
+    __u32 ip;
+    __u16 port;
+} __attribute__((packed));
+typedef struct conntrack_entry ce;
+
+#define SNAT_MAP_SIZE 1024
+#define DNAT_MAP_SIZE 1024
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, SNAT_MAP_SIZE);
+    __type(key, ce);
+    __type(value, ce);
+} snat_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, DNAT_MAP_SIZE);
+    __type(key, ce);
+    __type(value, ce);
+} dnat_map SEC(".maps");
+
+enum LB_ALG{
+    random, round_robin
+};
 
 #ifndef memcpy
 #define memcpy(dest, src, n) __builtin_memcpy((dest), (src), (n))
@@ -34,9 +71,22 @@ csum_fold_helper(__u64 csum)
 }
 
 static __always_inline __u16
-iph_csum(struct iphdr *iph)
-{
+iph_csum(struct iphdr *iph){
     iph->check = 0;
     unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int *)iph, sizeof(struct iphdr), 0);
     return csum_fold_helper(csum);
+}
+
+static __always_inline __u16 
+ipv4_l4_csum(void* data_start, __u32 data_size, struct iphdr* iph) {
+    __u32 tmp = 0;
+    __u64* csum = 0;
+    *csum = bpf_csum_diff(0, 0, &iph->saddr, sizeof(__be32), *csum);
+    *csum = bpf_csum_diff(0, 0, &iph->daddr, sizeof(__be32), *csum);
+    tmp = __builtin_bswap32((__u32)(iph->protocol));
+    *csum = bpf_csum_diff(0, 0, &tmp, sizeof(__u32), *csum);
+    tmp = __builtin_bswap32((__u32)(data_size));
+    *csum = bpf_csum_diff(0, 0, &tmp, sizeof(__u32), *csum);
+    *csum = bpf_csum_diff(0, 0, data_start, data_size, *csum);
+    return csum_fold_helper(*csum);
 }
